1268564daf08079419f285c197ea792860050b32
TheWillGabriel <will.gabriel@gmail.com>
2019-10-01 15:18:47 -0600

    Calculate age in years

--- space-age/space_age.rb
+++ space-age/space_age.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
 class SpaceAge
-  @relative_orbital_periods = { earth: 1,
-                                mercury: 0.2408467,
-                                venus: 0.61519726,
-                                mars: 1.8808158,
-                                jupiter: 11.862615,
-                                saturn: 29.447498,
-                                uranus: 84.016846,
-                                neptune: 164.79132 }
+  RELATIVE_ORBITAL_PERIODS = { earth: 1,
+                               mercury: 0.2408467,
+                               venus: 0.61519726,
+                               mars: 1.8808158,
+                               jupiter: 11.862615,
+                               saturn: 29.447498,
+                               uranus: 84.016846,
+                               neptune: 164.79132 }
 
   def initialize(seconds)
-    @age_in_seconds = seconds
+    @age_in_seconds = seconds.to_f
+    @age_in_years = @age_in_seconds / 31_557_600
   end
 end
