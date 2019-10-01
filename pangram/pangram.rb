d98de19c0b09e11b12b00f0b6df44abd3d065538
TheWillGabriel <will.gabriel@gmail.com>
2019-10-01 14:24:29 -0600

    Scan for unique downcased letters using regex and sort

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
-ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+ALPHABET = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
 
 class Pangram
   def self.pangram?(sentence)
-    ALPHABET == sentence
+    letters = sentence.downcase.scan(/[a-z]/).uniq.sort
+    ALPHABET == letters
   end
 end
