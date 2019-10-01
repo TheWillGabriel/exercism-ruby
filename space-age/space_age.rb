4c758a24744429c13cd9627a36dfadbce9643296
TheWillGabriel <will.gabriel@gmail.com>
2019-10-01 15:20:15 -0600

    Move age in years to earth method

--- space-age/space_age.rb
+++ space-age/space_age.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class SpaceAge
-  RELATIVE_ORBITAL_PERIODS = { earth: 1,
-                               mercury: 0.2408467,
+  RELATIVE_ORBITAL_PERIODS = { mercury: 0.2408467,
                                venus: 0.61519726,
                                mars: 1.8808158,
                                jupiter: 11.862615,
@@ -12,6 +11,9 @@ class SpaceAge
 
   def initialize(seconds)
     @age_in_seconds = seconds.to_f
-    @age_in_years = @age_in_seconds / 31_557_600
+  end
+
+  def on_earth
+    @age_in_seconds / 31_557_600
   end
 end
