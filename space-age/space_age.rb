830d05d96df43b442475c094d34db24de8f8502f
TheWillGabriel <will.gabriel@gmail.com>
2019-10-01 15:08:49 -0600

    Define hash of relative orbital periods

--- space-age/space_age.rb
+++ space-age/space_age.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 class SpaceAge
+  @relative_orbital_periods = { earth: 1,
+                                mercury: 0.2408467,
+                                venus: 0.61519726,
+                                mars: 1.8808158,
+                                jupiter: 11.862615,
+                                saturn: 29.447498,
+                                uranus: 84.016846,
+                                neptune: 164.79132 }
+
   def initialize(seconds)
     @age_in_seconds = seconds
   end
