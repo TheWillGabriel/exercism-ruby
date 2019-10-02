976c35aaf50f8cc56c0f55efe7040d3289221bea
TheWillGabriel <will.gabriel@gmail.com>
2019-10-01 20:27:46 -0600

    Add isolsceles method

--- triangle/triangle.rb
+++ triangle/triangle.rb
@@ -3,9 +3,17 @@ class Triangle
     @sides = sides
   end
 
-  def equilateral?
+  def triangle?
     return false if @sides.any?(0)
 
-    @sides.all?(@sides[0])
+    @sides.max <= @sides.sort.first(2).sum
+  end
+
+  def equilateral?
+    triangle? && @sides.all?(@sides[0])
+  end
+
+  def isosceles?
+    triangle? && (@sides.count(@sides[0]) > 1 || @sides.count(@sides[1]) > 1)
   end
 end
