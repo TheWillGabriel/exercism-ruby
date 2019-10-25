defbf0f5343a18d4421f4fc7c4386fca607027ab
TheWillGabriel <will.gabriel@gmail.com>
2019-10-24 19:16:05 -0600

    Raise ArgumentError on too-long or non-int argument

--- series/series.rb
+++ series/series.rb
@@ -4,9 +4,12 @@ class Series
   end
 
   def slices(slice_length)
+    raise ArgumentError unless slice_length.is_a?(Integer) &&
+                               slice_length <= @digits.length
+
     slice_array = []
     (@digits.length - slice_length + 1).times do |index|
-      slice_array << @digits[index..(slice_length + index - 1)]
+      slice_array << @digits[index..(index + slice_length - 1)]
     end
     slice_array
   end
