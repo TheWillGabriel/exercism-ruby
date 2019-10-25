b3706eb03ddd07fd407c782052a9a69ae6c40672
TheWillGabriel <will.gabriel@gmail.com>
2019-10-24 18:14:39 -0600

    Use #lines instead of #split to separate rows

--- matrix/matrix.rb
+++ matrix/matrix.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 class Matrix
-  attr_reader :rows
-  attr_reader :columns
+  attr_reader :rows, :columns
 
   def initialize(string)
-    @rows = string.split("\n").map { |row| row.split.map(&:to_i) }
+    @rows = string.lines.map { |row| row.split.map(&:to_i) }
     @columns = @rows.transpose
   end
 end
